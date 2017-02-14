ea6cfb73a7bd2aa4f92c8f73ebc3258531671880
php 12 <php.12@metralha.cec.ime.usp.br>
2017-02-14 20:54:06 -0200

    view 5

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+{{-- <html>
   <head>
     <meta charset="utf-8">
     <title>Curso</title>
@@ -17,4 +17,14 @@
 
     <script type="text/javascript" src="/js/app.js">
   </body>
-</html>
+</html> --}}
+
+ @extends('layout')
+
+ @section('content')
+  <div class="row">
+      <div class="col-md-6">
+        <h4>{{ $curso }}</h4>
+      </div>
+  </div>
+ @endsection
